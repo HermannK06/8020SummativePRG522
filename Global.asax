74f2d1cf6f3821310d137ee1fd1799e0dcbd3586
HermannK06 <8020@ctucareer.co.za>
2021-09-15 11:15:25 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_8020SummativePRG522.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_8020SummativePRG522.Startup))]
+namespace _8020SummativePRG522
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
